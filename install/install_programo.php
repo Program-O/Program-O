7eaf9b66baae4d2b0652d67254333bf37779cf26
Dave Morton <dmorton@geekcavecreations.com>
2012-04-28 00:25:20 -0700

    Major bug fix Located and corrected the issue where certain data was not being written correctly into the config file.
    Signed-off-by: Dave Morton <dmorton@geekcavecreations.com>

--- install/install_programo.php
+++ install/install_programo.php
@@ -1,7 +1,7 @@
 <?PHP
 //-----------------------------------------------------------------------------------------------
 //My Program-O Version 2.0.1
-//Program-O  chatbot admin area
+//Program-O chatbot admin area
 //Written by Elizabeth Perreau and Dave Morton
 //May 2011
 //for more information and support please visit www.program-o.com
@@ -15,44 +15,51 @@ session_start();
 $_SESSION['errorMessage'] = (!empty($_SESSION['errorMessage'])) ? $_SESSION['errorMessage'] : '';
 if (!is_writable('../config/global_config.php')) {
   // Read and write for owner, read for everybody else
-  chmod('../config/global_config.php', 0777);
-  if (!is_writable('../config/global_config.php')) $_SESSION['errorMessage'] .= "Please make the /config/global_config.php writeable.";
+  if (!chmod('../config/global_config.php', 0755)) $_SESSION['errorMessage'] .= 'Please check the file permissions for /config/global_config.php  see the <a href="help.php?page=1#permissions">Help Page</a> for more information.';
 }
 require_once('../library/buildSelect.php');
 require_once('../config/global_config.php');
+
 define ('SECTION_START', '<!-- Section [section] Start -->'); # search params for start and end of sections
 define ('SECTION_END', '<!-- Section [section] End -->'); # search params for start and end of sections
 define ('PHP_SELF', $_SERVER['SCRIPT_NAME']); # search params for start and end of sections
-ini_set("display_errors", 1);
-ini_set("log_errors",true);
-#ini_set("error_log","../logs/error.log");
-ini_set("error_log","error.log");
+ini_set("display_errors", 0);
+ini_set("log_errors", true);
+ini_set("error_log", _INSTALL_PATH_ . "error.log");
+if (!file_exists(_INSTALL_PATH_ . "error.log")) file_put_contents(_INSTALL_PATH_ . "error.log", '');
 
 $currentDir = getcwd();
 $defaultBaseDir = rtrim(str_ireplace('admin', '', $currentDir), '\\/');
 $sep = (substr($defaultBaseDir,0, 1) == '/') ? '/' : '\\';
 
-$localhost = $remotehost = $local_dbh = $remote_dbh = $local_dbn = $remote_dbn = $local_dbu = $remote_dbu = $local_dbp = $remote_dbp = 'Required';
 $myHost = $_SERVER['SERVER_NAME'];
 $domain_name = $myHost;
 switch ($myHost) {
   case 'localhost':
   case $alternate_local_server_name:
   $localhost = $myHost;
+/*
   $local_dbh = $dbh;
   $local_dbn = $dbn;
   $local_dbu = $dbu;
   $local_dbp = $dbp;
-   break;
+*/
+  $hostLocation = 'local';
+  break;
   default:
   $remotehost = $myHost;
+/*
   $remote_dbh = $dbh;
   $remote_dbn = $dbn;
   $remote_dbu = $dbu;
   $remote_dbp = $dbp;
+*/
+  $hostLocation = 'remote';
 }
+if (empty($dbu)) $dbu = $_SESSION[$hostLocation . '_dbu'];
+if (empty($dbp)) $dbp = $_SESSION[$hostLocation . '_dbp'];
 
-$replTagsArray = file('../install/config_template_tags.dat', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+$replTagsArray = file(_INSTALL_PATH_ . 'config_template_tags.dat', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 $replVarsArray = array();
 foreach ($replTagsArray as $value) {
   $value = trim($value);
@@ -64,23 +71,18 @@ chdir(dirname( realpath( __FILE__ )));
 $rsTzVal = (function_exists('date_default_timezone_get')) ? date_default_timezone_get() : '';
 $validPages = array(1,2,3);
 $page_template = file_get_contents('config.tpl.htm');
-if (isset($_POST)) {
-  foreach ($_POST as $key => $value) {
-  $_SESSION[$key] = rtrim($value);
- }
-}
 $page = (isset($_REQUEST['page'])) ? $_REQUEST['page'] : 1;
 if (!in_array($page, $validPages)) $page = 1;
 $func = (isset($_REQUEST['func'])) ? $_REQUEST['func'] : '';
 if (!empty($func)) $nextPage = $func($page);
 $pageTemplate = 'container';
 $pageNotes = ucwords("Page $page Notes");
-$content    = getSection('Header', $page_template, false);
-$content .= "<!-- myHost = $myHost, localhost = $localhost  -->\n";
-$content    .= getSection($pageTemplate, $page_template);
-$content    .= getSection('Footer', $page_template);
-$content    .= getSection("jQuery$page", $page_template);
-$notes     = getSection($pageNotes, $page_template);
+$content = getSection('Header', $page_template, false);
+$content .= "<!-- myHost = $myHost, localhost = $localhost -->\n";
+$content .= getSection($pageTemplate, $page_template);
+$content .= getSection('Footer', $page_template);
+$content .= getSection("jQuery$page", $page_template);
+$notes = getSection($pageNotes, $page_template);
 $submitButton = getSection('SubmitButton', $page_template);
 $main = getMain($page, $page_template);
 $tmpSearchArray = array();
@@ -98,13 +100,13 @@ foreach ($replVarsArray as $search => $replace) {
   $replace = trim($replace);
   if (!isset($$replace)) $$replace = '';
   $repl1 = $$replace;
-  if (isset($$replace) and $$replace != '') {
+  if (isset($$replace) and (!empty($$replace))) {
     $content = str_replace($search, $$replace, $content);
   }
 }
 if ($page == 2) $content = str_replace('[BotsTableForm]', page2form(), $content);
-$content = str_replace('[cr6]', "\n    ", $content);
-$content = str_replace('[cr4]', "\n   ", $content);
+$content = str_replace('[cr6]', "\n ", $content);
+$content = str_replace('[cr4]', "\n ", $content);
 $content = str_replace('[nextPage]', $page + 1, $content);
 $content = str_replace("\r\n", "\n", $content);
 $content = str_replace("\n\n", "\n", $content);
@@ -115,7 +117,7 @@ foreach ($replVarsArray as $key => $value) {
 }
 $content .= <<<endPage
 
- </body>
+</body>
 </html>
 endPage;
 
@@ -124,7 +126,7 @@ exit($content);
 function getSection($sectionName, $page_template, $notFoundReturn = true) {
   $sectionStart = str_replace('[section]', $sectionName, SECTION_START);
   $sectionStartLen = strlen($sectionStart);
-  $sectionEnd   = str_replace('[section]', $sectionName, SECTION_END);
+  $sectionEnd = str_replace('[section]', $sectionName, SECTION_END);
   $startPos = strpos($page_template, $sectionStart, 0);
    if ($startPos === false) {
      if ($notFoundReturn) {
@@ -140,10 +142,15 @@ function getSection($sectionName, $page_template, $notFoundReturn = true) {
 }
 
 function save($page) {
-  global $replVarsArray, $quickdebug, $writetotemp;
-  $curSession = print_r($_SESSION, true);
-  #if (!checkDBContents($dbn)) fillDB();
-  checkDBContents($_SESSION['local_dbn']);
+  global $replVarsArray, $quickdebug, $writetotemp, $hostLocation, $postVars;
+  $postVars = '';
+  $postVars = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+  ksort($postVars);
+  foreach ($postVars as $key => $value) {
+    $_SESSION[$key] = rtrim($value);
+  }
+  $x = file_put_contents(_INSTALL_PATH_ . 'sessionVars.txt', print_r($_SESSION, true) . "\r\n");
+  checkDBContents($postVars);
   switch ($page) {
     case 1:
       $out = 2;
@@ -173,7 +180,7 @@ function save($page) {
           $botArrayTemplate = rtrim($botArrayTemplate, ',') . ');';
           $value = $botArrayTemplate . "\r\n";
         }
-        $repl = (isset($_SESSION[$value])) ? $_SESSION[$value] : '';
+        $repl = (isset($_SESSION[$value])) ? $_SESSION[$value] : 'missing';
         $newConfigFile = str_replace($key, $repl, $newConfigFile);
         $newConfigFile = str_replace("\r\n", "\n", $newConfigFile);
       }
@@ -187,7 +194,7 @@ function save($page) {
 }
 
 function getMain($page, $page_template) {
-  global $dbh, $dbu, $dbp, $dbn, $dbPort;
+  global $dbh, $dbu, $dbp, $dbn, $dbPort, $adm_dbu, $adm_dbp;
   switch ($page) {
     case 1:
       return getSection('GeneralConfig',$page_template);
@@ -198,7 +205,7 @@ function getMain($page, $page_template) {
   }
   $numBots = $_SESSION['bot_count'];
   $sql_template = <<<endSQL
-INSERT IGNORE INTO `bots` (`bot_id`, `bot_name`, `bot_desc`, `bot_active`, `bot_parent_id`, `format`, `use_aiml_code`, `update_aiml_code`, `save_state` , `conversation_lines` , `remember_up_to` , `debugemail`, `debugshow`, `debugmode`, `default_aiml_pattern`) VALUES ([cur_bot_ID], '[cur_bot_name]', '[cur_bot_desc]', [cur_bot_active], [cur_bot_parent_id], '[cur_format]', '[cur_save_state]', [cur_conversation_lines], [cur_remember_up_to], '[cur_debugemail]',  [cur_debugshow], [cur_debugmode], '[cur_default_aiml_pattern]', [cur_use_aiml_code], [cur_update_aiml_code]);
+INSERT IGNORE INTO `bots` (`bot_id`, `bot_name`, `bot_desc`, `bot_active`, `bot_parent_id`, `format`, `use_aiml_code`, `update_aiml_code`, `save_state` , `conversation_lines` , `remember_up_to` , `debugemail`, `debugshow`, `debugmode`, `default_aiml_pattern`) VALUES ([cur_bot_ID], '[cur_bot_name]', '[cur_bot_desc]', [cur_bot_active], [cur_bot_parent_id], '[cur_format]', '[cur_save_state]', [cur_conversation_lines], [cur_remember_up_to], '[cur_debugemail]', [cur_debugshow], [cur_debugmode], '[cur_default_aiml_pattern]', [cur_use_aiml_code], [cur_update_aiml_code]);
 endSQL;
   require_once ('../library/error_functions.php');
   require_once ('../library/db_functions.php');
@@ -210,10 +217,12 @@ endSQL;
     $sql = @str_replace('[cur_bot_active]',$_SESSION["bot_active_$loop"], $sql);
     $sql = @str_replace('[cur_bot_parent_id]',$_SESSION['default_bot_id'], $sql);
     $sql = @str_replace('[cur_format]',$_SESSION["format_$loop"], $sql);
+    // "Use PHP from DB setting
     if (!isset($_SESSION["use_aiml_code_$loop"])) $_SESSION["use_aiml_code_$loop"] = 0;
-    $cur_use_aiml_code_checked = ($_SESSION["use_aiml_code_$loop"] == 1) ? ' checked="checked"' : '';
     $sql = str_replace('[cur_use_aiml_code]',$_SESSION["use_aiml_code_$loop"], $sql);
-    $sql = str_replace('[cur_use_aiml_code_checked]',$cur_use_aiml_code_checked, $sql);
+    $cur_use_aiml_code_checked = ($_SESSION["use_aiml_code_$loop"] == 1) ? ' checked="checked"' : '';
+    $sql = str_replace('[cur_use_aiml_code_checked]', $cur_use_aiml_code_checked, $sql);
+    // "Update PHP in DB setting
     if (!isset($_SESSION["update_aiml_code_$loop"])) $_SESSION["update_aiml_code_$loop"] = 0;
     $cur_update_aiml_code_checked = ($_SESSION["update_aiml_code_$loop"] == 1) ? ' checked="checked"' : '';
     $sql = str_replace('[cur_update_aiml_code]',$_SESSION["update_aiml_code_$loop"], $sql);
@@ -222,16 +231,16 @@ endSQL;
     $sql = @str_replace('[cur_conversation_lines]',$_SESSION["conversation_lines_$loop"], $sql);
     $sql = @str_replace('[cur_remember_up_to]',$_SESSION["remember_up_to_$loop"], $sql);
     $sql = str_replace('[cur_debugemail]',$_SESSION["default_debugemail"], $sql);
+    $sql = str_replace('[default_debugemail]',$_SESSION["default_debugemail"], $sql);
     $sql = str_replace('[cur_debugshow]',$_SESSION["default_debugshow"], $sql);
     $sql = str_replace('[cur_debugmode]',$_SESSION["default_debugmode"], $sql);
     $sql = str_replace('[cur_default_aiml_pattern]',$_SESSION["default_pattern"], $sql);
     $x = db_query($sql, $conn) or $_SESSION['errorMessage'] = 'Could not enter bot info for bot #' . $loop . '! Error = ' . mysql_error();
   }
-  global $adm_dbu, $adm_dbp;
   $encrypted_adm_dbp = md5($adm_dbp);
   $cur_ip = $_SERVER['REMOTE_ADDR'];
   $adminSQL = "insert ignore into `myprogramo` (`id`, `uname`, `pword`, `lastip`) values(null, '$adm_dbu', '$encrypted_adm_dbp', '$cur_ip');";
-  $result = db_query($adminSQL, $conn) or $_SESSION['errorMessage'] = 'Could not add admin account! Error = ' . mysql_error();
+  $result = db_query($adminSQL, $conn) or $_SESSION['errorMessage'] = 'Could not add admin account! Error = ' . mysql_error() . "user = $adm_dbu, pass = $adm_dbp<br>\n";
   return ($result) ? getSection('InstallComplete', $page_template) : getSection('InstallError', $page_template);
 }
 
@@ -240,37 +249,52 @@ function page2form() {
   $out = '';
   $namesArray = explode("\n", $_SESSION['botNames']);
   for ($loop = 1; $loop <= $_SESSION['bot_count']; $loop++) {
+    $cur_use_aiml_code_checked = ($_SESSION["use_aiml_code_$loop"] == 1) ? ' checked="checked"' : '';
+    $cur_update_aiml_code_checked = ($_SESSION["update_aiml_code_$loop"] == 1) ? ' checked="checked"' : '';
     $curConvoLines = (!empty($_SESSION["conversation_lines_$loop"])) ? $_SESSION["conversation_lines_$loop"] : $_SESSION['default_conversation_lines'];
     $tmpSection = getSection('BotsTableForm', $page_template);
     $tmpSection = str_replace('[bot_ID]', $loop, $tmpSection);
     $tmpSection = str_replace('[current_bot_name]', rtrim($namesArray[$loop-1]), $tmpSection);
+    $tmpSection = str_replace('[cur_use_aiml_code_checked]', $cur_use_aiml_code_checked, $tmpSection);
+    $tmpSection = str_replace('[cur_update_aiml_code_checked]', $cur_update_aiml_code_checked, $tmpSection);
     $tmpSection = str_replace('[cl_value]', $curConvoLines, $tmpSection);
     $out .= $tmpSection;
   }
   return $out;
 }
 
-function checkDBContents($dbn) {
-  $local_dbh    = $_SESSION['local_dbh'];
-  $local_dbn    = $_SESSION['local_dbn'];
-  $local_dbu    = $_SESSION['local_dbu'];
-  $local_dbp    = $_SESSION['local_dbp'];
-  $local_dbPort = $_SESSION['local_dbPort'];
+function checkDBContents($postVars) {
+  global $hostLocation;
+  $x = file_put_contents(_INSTALL_PATH_ . $hostLocation . '_postVars.txt', print_r($postVars, true) . "\r\n");
+  switch ($hostLocation) {
+    case 'local':
+      $tmpDbh = $_SESSION['local_dbh'];
+      $tmpDbn = $_SESSION['local_dbn'];
+      $tmpDbu = $_SESSION['local_dbu'];
+      $tmpDbp = $_SESSION['local_dbp'];
+    default:
+      $tmpDbh = $_SESSION['remote_dbh'];
+      $tmpDbn = $_SESSION['remote_dbn'];
+      $tmpDbu = $_SESSION['remote_dbu'];
+      $tmpDbp = $_SESSION['remote_dbp'];
+  }
+
   $death = <<<endDeath
 
 <pre>
 Variables:
-local_dbh: $local_dbh
-local_dbu: $local_dbu
-local_dbp: $local_dbp
-local_dbn: $local_dbn
-local_dbPort: $local_dbPort
+Host Location: $hostLocation
+dbh: $tmpDbh
+dbu: $tmpDbu
+dbp: $tmpDbp
+dbn: $tmpDbn
+dbPort: $tmpDbPort
 
 
 endDeath;
-  $host = ($local_dbh == 'Required') ? 'localhost' : $local_dbh;
-  $conn = mysql_connect($host, $local_dbu, $local_dbp) or die( "mysql_connect error:". mysql_errno() . ', ' . mysql_error() . $death);
-  mysql_select_db($local_dbn,$conn);
+
+  $conn = mysql_connect($tmpDbh, $tmpDbu, $tmpDbp) or die( "mysql_connect error:". mysql_errno() . ', ' . mysql_error() . " at line " . __LINE__ . ' of file ' . __FILE__ . ' in function ' . __FUNCTION__ . '.' . $death);
+  mysql_select_db($tmpDbn,$conn);
   $sql = "show tables;";
   $result = mysql_query($sql,$conn) or die ("Houston, we have a problem! " . mysql_error() . ", sql = $sql");
   $out = mysql_fetch_assoc($result);
