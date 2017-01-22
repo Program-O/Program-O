6b6f419c86d3b660f2ede00cb94b64bd7a99403d
Patrick Schur <patrick_schur@outlook.de>
2017-01-22 22:03:13 +0100

    Added German translation files

---
+++ lang/de-de/lang.php
@@ -0,0 +1,15 @@
+<?php
+  /***************************************
+  * http://www.program-o.com
+  * PROGRAM O
+  * Version: 2.7.0
+  * FILE: lang.php
+  * AUTHOR: Elizabeth Perreau and Dave Morton
+  * DATE: 01/20/2017
+  * DETAILS: Contains the text strings for Program O in US English
+  ***************************************/
+
+  $lang = array(
+    'botPersonalityUpdated' => 'Bot Personality Updated.',
+
+  );
