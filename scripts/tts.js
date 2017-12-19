fff73eace8710850a47fc86fa4067084d34bed92
Patrick Schur <patrick_schur@outlook.de>
2017-12-19 23:09:04 +0100

    Added check for speechSynthesis support

--- scripts/tts.js
+++ scripts/tts.js
@@ -1,5 +1,7 @@
 const speak = function (message) {
-    const msg = new SpeechSynthesisUtterance(message);
-    msg.voice = window.speechSynthesis.getVoices()[0];
-    window.speechSynthesis.speak(msg);
+    if ("speechSynthesis" in window) {
+        const msg = new SpeechSynthesisUtterance(message);
+        msg.voice = window.speechSynthesis.getVoices()[0];
+        window.speechSynthesis.speak(msg);
+    }
 };
