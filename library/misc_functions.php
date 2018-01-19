c1d1e7fbb2b0767c5c75492bb2bbb59435190016
Patrick Schur <patrick_schur@outlook.de>
2018-01-19 22:09:10 +0100

    Fix for issue #351

--- library/misc_functions.php
+++ library/misc_functions.php
@@ -254,61 +254,17 @@ function pretty_print_r($var)
 
 function clean_inputs($allowed_vars = null)
 {
-    $options = null; //Temp fix for now. Must assign something other than null to this at some point
-    $out = '';
-
-/* Debugging tool - see what's coming into the form vars, and which filters (if any) are being applied
-    $filterVals = array(
-        FILTER_SANITIZE_STRIPPED      => 'FILTER_SANITIZE_STRIPPED',
-        FILTER_SANITIZE_STRING        => 'FILTER_SANITIZE_STRING',
-        FILTER_SANITIZE_ENCODED       => 'FILTER_SANITIZE_ENCODED',
-        FILTER_SANITIZE_SPECIAL_CHARS => 'FILTER_SANITIZE_SPECIAL_CHARS',
-        FILTER_SANITIZE_EMAIL         => 'FILTER_SANITIZE_EMAIL',
-        FILTER_SANITIZE_URL           => 'FILTER_SANITIZE_URL',
-        FILTER_SANITIZE_NUMBER_INT    => 'FILTER_SANITIZE_NUMBER_INT',
-        FILTER_SANITIZE_NUMBER_FLOAT  => 'FILTER_SANITIZE_NUMBER_FLOAT',
-        FILTER_SANITIZE_MAGIC_QUOTES  => 'FILTER_SANITIZE_MAGIC_QUOTES',
-        FILTER_UNSAFE_RAW             => 'FILTER_UNSAFE_RAW',
-        FILTER_DEFAULT                => 'FILTER_DEFAULT',
-    );
-    if (!defined('ERROR_DEBUGGING')) define('ERROR_DEBUGGING', false); //just in case a fresh install hasn't been performed for some reason
-
-    if (ERROR_DEBUGGING) // This constant may be defined in the config. Must be set to true for this to take effect.
+    $formVars = array();
+    if (!empty($allowed_vars))
     {
-        $ts = str_replace(' ', '_', microtime());
-        save_file(_LOG_PATH_ . "misc_functions.clean_inputs.raw_formVars.{$ts}.txt", print_r($formVars, true));
+        foreach ($allowed_vars as $key => $value){
+            if (!isset($_REQUEST[$key])) continue;
+            $curVar = $_REQUEST[$key];
+            $val = (is_array($curVar))? filter_var_array($curVar, $value) : filter_var(trim($curVar), $value);
+            $formVars[$key] = $val;
+        }
+        $out = $formVars;
     }
-*/
-    $allowed_local_hosts = array(
-        '127.0.0.1',
-        '::1',
-        'localhost'
-    );
-    $referer = (isset($_SERVER['HTTP_REFERER'])) ? $_SERVER['HTTP_REFERER'] : false;
-    $host = (isset($_SERVER['HTTP_HOST'])) ? $_SERVER['HTTP_HOST'] : false;
-    if (false === $host || (false === $referer && (!in_array($host, $allowed_local_hosts)))) die ('CSRF failure!');
-    $formVars = array_merge($_GET, $_POST);
-    foreach ($formVars as $key => $value)
-    {
-        $filter = (is_array($allowed_vars) && array_key_exists($key, $allowed_vars)) ? $allowed_vars[$key] : FILTER_DEFAULT;
-        $out[$key] = clean_var($value, $filter, $options);
-    }
+    else $out = filter_var_array($_REQUEST);
     return $out;
 }
-
-function clean_var($var, $filter = FILTER_SANITIZE_STRING, $options = null)
-{
-    if (!is_array($var)) return filter_var($var, $filter, $options);
-    $out = array();
-    foreach ($var as $key => $value)
-    {
-        $out[$key] = clean_var($value, $filter, $options);
-    }
-    return $out;
-}
-
-
-
-
-
-
